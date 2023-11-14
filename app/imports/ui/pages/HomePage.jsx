aa13c10ed8748c4dbc26fbbe835512613208f693
Joshua Lorica <loricaj@hawaii.edu>
2023-11-14 13:07:19 -1000

    Updated Landing Page

---
+++ app/imports/ui/pages/HomePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import MainVendors from '../components/MainVendors';
+import MainFood from '../components/MainFood';
+import RecentlyOrdered from '../components/RecentlyOrdered';
+
+/* A simple static component to render some text for the landing page. */
+const Landing = () => (
+  <Container id="landing-page" fluid className="py-3">
+    <Container>
+      <Row>
+        <MainVendors />
+      </Row>
+    </Container>
+    <Container style={{ marginTop: '25px' }}>
+      <Row>
+        <MainFood />
+      </Row>
+    </Container>
+    <Container style={{ marginTop: '25px' }}>
+      <Row>
+        <RecentlyOrdered />
+      </Row>
+    </Container>
+  </Container>
+);
+
+export default Landing;
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import { Container, Row } from 'react-bootstrap';
-import MainVendors from '../components/MainVendors';
-import MainFood from '../components/MainFood';
-import RecentlyOrdered from '../components/RecentlyOrdered';
-
+import { Col, Container, Row } from 'react-bootstrap';
+import { EggFried, MapFill, GlobeAmericas } from 'react-bootstrap-icons';
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Container>
-      <Row>
-        <MainVendors />
-      </Row>
-    </Container>
-    <Container style={{ marginTop: '25px' }}>
-      <Row>
-        <MainFood />
-      </Row>
-    </Container>
-    <Container style={{ marginTop: '25px' }}>
-      <Row>
-        <RecentlyOrdered />
-      </Row>
-    </Container>
+  <Container id="landing-page" className="py-3">
+    <Row className="align-middle text-center">
+      <h1>Welcome to Food Now!</h1>
+      <h2>Looking for food on Campus? We can help!</h2>
+      <h4>Search by..</h4>
+
+      <Col xs={4}>
+        <EggFried size={100} />
+        <h1>Vendor List</h1>
+        <h5>List of all food options on campus. </h5>
+      </Col>
+
+      <Col xs={4}>
+        <MapFill size={100} />
+        <h1>Vendor Map</h1>
+        <h5>View food options on Campus Map.</h5>
+      </Col>
+
+      <Col xs={4}>
+        <GlobeAmericas size={100} />
+        <h1>Cuisine Type</h1>
+        <h5>Search vendors by cuisine type.</h5>
+      </Col>
+
+      <h4>And more!..</h4>
+      <h4>Login below to find your next meal</h4>
+
+    </Row>
   </Container>
 );
 
