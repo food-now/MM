8f0a4be26bda64386cd5b293f76d659c19e5d832
kairemUH <100346220+kairemUH@users.noreply.github.com>
2023-11-14 15:27:34 -1000

    Created MenuItem collection

---
+++ app/imports/api/MenuItem/MenuItem.js
@@ -0,0 +1,45 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/**
+ * The MenuItemsCollection. It encapsulates state and variable values for stuff.
+ */
+class MenuItemsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'MenuItemsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema(
+      {
+        owner: String,
+        dateCreated: Date,
+        vendorName: String,
+        name: String,
+        price: Number,
+        allergens: [String],
+        daysOfWeekAvaliable: [String],
+        special: Boolean,
+        specialDate: Date,
+        image: {
+          type: String,
+          defaultValue: '',
+          required: false,
+        },
+      },
+      { requiredByDefault: true },
+    );
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the MenuItemsCollection.
+ * @type {MenuItemsCollection}
+ */
+export const MenuItems = new MenuItemsCollection();
