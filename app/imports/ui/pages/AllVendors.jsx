7d897eee55be703a5ce69ae6bb146a2124989927
gavyngostage <ggostage@hawaii.edu>
2023-12-05 01:13:58 -1000

    added scrolling container for vendors and other minor styling changes

--- app/imports/ui/pages/AllVendors.jsx
+++ app/imports/ui/pages/AllVendors.jsx
@@ -33,7 +33,7 @@ const ListStuff = () => {
     <Container id="AllVendors-page" fluid className="py-5">
       <Row>
         {/* Left Block for Filters */}
-        <Col md={2}>
+        <Col className="col-auto py-2">
           {/* Your filter components go here */}
           <h2>Filters</h2>
           <VendorFilter />
@@ -42,17 +42,19 @@ const ListStuff = () => {
         {/* Middle Block for Section */}
         <Col md={6}>
           {/* Your main section components go here */}
-          <h1>Vendors Available at UH Manoa</h1>
-          {vendors.map((vendor, index) => (
-            <VendorsCard vendor={vendor} key={index} />
-          ))}
+          <h1 className="text-center mb-3">Vendors Available at UH Manoa</h1>
+          <div className="vendor-list">
+            {vendors.map((vendor, index) => (
+              <VendorsCard vendor={vendor} key={index} />
+            ))}
+          </div>
           {/* Add your main section components or content here */}
         </Col>
 
         {/* Right Block for Section */}
-        <Col md={3}>
+        <Col className="col-4 py-2">
           {/* Your right section components go here */}
-          <h2>Find Your Favorite Spots</h2>
+          <h2 className="justify-content-center mb-3">Find Your Favorite Spots</h2>
           <InteractiveMap />
           {/* Add your right section components or content here */}
         </Col>
