70edbdfdad9b5ec9be46a2be5017c170a5d0a0e2
Connor Narowetz <cnarowetz@gmail.com>
2023-12-07 12:27:03 -1000

    added map items (finished map)

---
+++ app/imports/ui/components/map-components/BeanCounterPopup.jsx
@@ -0,0 +1,13 @@
+// PopupContent.js
+import React from 'react';
+
+const BeanCounterPopup = () => {
+  return (
+    <div>
+      <h4>🏢 Business Admin Building ☕</h4>
+      <h6><a href="http://localhost:3000/show-vendor/raTHrGAfz2aA7Bnyd">The Bean Counter</a></h6>
+    </div>
+  );
+};
+
+export default BeanCounterPopup;
