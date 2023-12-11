36429277e4308b1754d13ba7d6840631f7c57756
tliam1 <tliam@hawaii.edu>
2023-12-10 20:24:21 -0600

    More Eslint Fixes

--- app/imports/ui/components/map-components/FoodTruckRowPopup.jsx
+++ app/imports/ui/components/map-components/FoodTruckRowPopup.jsx
@@ -1,19 +1,17 @@
 // PopupContent.js
 import React from 'react';
 
-const FoodTruckRowPopup = () => {
-  return (
-    <div>
-      <h3>🍱 Food Truck Row! 🚚</h3>
-      <h6><a href="http://localhost:3000/show-vendor/xkuQfmgQBy2dQ5RtT">Aloha Coffee</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/mpNdnZXYfYJrpu4rt">Evisuya</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/SDZJAWi4ubk5FFGw7">Happy Belly</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/gefGaji72smDg5D4P">HI Cravings</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/srMqni9C8RRchYTzk">Holoholo Bistro</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/xrm3r9mZumm9Bmoc2">Sama Sama</a></h6>
-      <h6><a href="http://localhost:3000/show-vendor/RDHS7jtNkCrqdQ2Sq">Veggi Dogs</a></h6>
-    </div>
-  );
-};
+const FoodTruckRowPopup = () => (
+  <div>
+    <h3>🍱 Food Truck Row! 🚚</h3>
+    <h6><a href="http://localhost:3000/show-vendor/xkuQfmgQBy2dQ5RtT">Aloha Coffee</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/mpNdnZXYfYJrpu4rt">Evisuya</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/SDZJAWi4ubk5FFGw7">Happy Belly</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/gefGaji72smDg5D4P">HI Cravings</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/srMqni9C8RRchYTzk">Holoholo Bistro</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/xrm3r9mZumm9Bmoc2">Sama Sama</a></h6>
+    <h6><a href="http://localhost:3000/show-vendor/RDHS7jtNkCrqdQ2Sq">Veggi Dogs</a></h6>
+  </div>
+);
 
 export default FoodTruckRowPopup;
